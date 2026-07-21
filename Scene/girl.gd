070e708a6870ccbd61fc7c22ee17b58f876c959f
Ankit soni <asoni.soni12@gmail.com>
2026-07-21 20:49:53 +0530

    setup the scene 1

--- Scene/girl.gd
+++ Scene/girl.gd
@@ -15,34 +15,40 @@ enum Scenes{
 }
 #endregion
 
-const SPEED := 30.0
+const SPEED := 20.0
 const JUMP_VELOCITY = -400.0
 
 @export var left_dir := -50.0
 @export var right_dir := 100.0
 
 var curr_scene: Scenes = Scenes.SCENE_1
-var target_distance: float
 var pause: bool
 var player_state: States = States.IDLE
+var target_distance: float
 
 @onready var actress: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
-	target_distance = global_position.x + right_dir
+	start_scene(Scenes.SCENE_1)
 
 func _physics_process(delta: float) -> void:
 	if pause:
 		return
-	
-	match curr_scene:
-		Scenes.SCENE_1:
-			scene_1(delta)
-			pass
+
+	manage_current_scene(delta)
 	
 	manage_state()
 	
 	move_and_slide()
+
+
+## Manage the Current Scene 
+func manage_current_scene(delta: float) -> void:
+	match curr_scene:
+		Scenes.SCENE_1:
+			scene_1(delta)
+		Scenes.SCENE_2:
+			scene_2(delta)
 
 
 ## Handle Change in the Actress State
@@ -51,6 +57,7 @@ func change_state(next_state: States ) -> void:
 		return
 	else:
 		player_state = next_state
+
 
 ## Manages the Current State of the Actress
 func manage_state() -> void:
@@ -63,18 +70,47 @@ func manage_state() -> void:
 			actress.play("run")
 
 
+## set the desired location target and stores value in target_distance
+func set_target(offset: float) -> void:
+	target_distance = global_position.x + offset
+
+
 ## Use to Walk Character to Target Area
-func walk_to(location: float, _delta: float) -> void:
+func walk_to(target: float) -> bool:
 	change_state(States.WALK)
 	
-	if location > global_position.x:
-		velocity.x = SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var distance := target - global_position.x
+	
+	if abs( distance ) > 2:
+		velocity.x = sign( distance ) * SPEED
+		return false
+	
+	velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	if velocity.x == 0:
 		change_state(States.IDLE)
+		return true
+	
+	return false
 
 
-func scene_1(delta: float) -> void:
-	walk_to(target_distance, delta)
+## Setup the Scenes before Starting
+func start_scene(scene: Scenes) -> void:
+	match scene:
+		Scenes.SCENE_1:
+			set_target(right_dir);
+		Scenes.SCENE_2:
+			set_target(left_dir)
+
+
+## scene 1
+func scene_1(_delta: float) -> void:
+	if walk_to(target_distance):
+		start_scene(Scenes.SCENE_2);
+		curr_scene = Scenes.SCENE_2
+
+
+## Scene 2
+func scene_2(_delta: float) -> void:
+	actress.flip_h = true
+	walk_to(target_distance)
