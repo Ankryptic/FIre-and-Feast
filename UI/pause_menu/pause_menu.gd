9a0e717b2dc06c127e43595ee6feb6278d6f1d42
Ankit soni <asoni.soni12@gmail.com>
2026-08-30 20:43:54 +0530

    add hover effect

--- UI/pause_menu/pause_menu.gd
+++ UI/pause_menu/pause_menu.gd
@@ -1,14 +1,20 @@
 class_name PauseMenu
 extends Control
 
-@onready var continue_btn: Button = $VBoxContainer/ContinueBtn
-@onready var settings_btn: Button = $VBoxContainer/SettingsBtn
-@onready var help_btn: Button = $VBoxContainer/HelpBtn
-@onready var quit_btn: Button = $VBoxContainer/QuitBtn
+@export var hover_property: Vector2 = Vector2(1.1, 1.1)
+
+@export var continue_btn: Button 
+@export var settings_btn: Button 
+@export var help_btn: Button
+@export var quit_btn: Button 
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	continue_btn.pressed.connect(_on_continue_button_pressed)
+	settings_btn.pressed.connect(_on_setting_button_pressed)
+	help_btn.pressed.connect(_on_help_button_pressed)
+	quit_btn.pressed.connect(_on_Quit_button_pressed)
+	
 	var all_btns = [continue_btn, settings_btn, help_btn, quit_btn]
 	
 	for button in all_btns:
@@ -16,11 +22,33 @@ func _ready() -> void:
 		button.mouse_exited.connect(_on_mouse_unhover.bind(button))
 
 
-func _on_mouse_hover(btn: Button) -> void:
-	print(btn.name)
+func _on_continue_button_pressed() -> void:
 	pass
-	
+
+func _on_setting_button_pressed() -> void:
+	pass
+
+func _on_help_button_pressed() -> void:
+	pass
+
+func _on_Quit_button_pressed() -> void:
+	pass
+
+
+#region Hover Effect
+func _on_mouse_hover(btn: Button) -> void:
+	_init_pivot_point(btn)
+	var tween = create_tween()
+	tween.tween_property(btn, "scale", hover_property, 0.12)
+
 
 func _on_mouse_unhover(btn: Button) -> void:
-	print("Unhover, " ,btn.name)
-	pass
+	_init_pivot_point(btn)
+	var tween = create_tween()
+	tween.tween_property(btn, "scale", Vector2.ONE, 0.12)
+
+
+func _init_pivot_point(button: Button) -> void:
+	button.pivot_offset = button.size / 2
+
+#endregion
